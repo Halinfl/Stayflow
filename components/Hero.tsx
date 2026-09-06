416935f986d184d267dd4924ccf78599ec97278b
Hal Vespucci <bungah@proton.me>
2026-09-06 13:57:41 -0400

    Revert "Update hero creator image to cabana lifestyle shot"
    This reverts commit 71bf4750065bbd050539f385a98577bb19d5f2ef.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,7 +34,7 @@ export default function Hero() {
         >
           <SafeImage
             src="/images/hero/creator.jpg"
-            alt="Guest at a resort poolside cabana"
+            alt="Creator shooting content on location"
             fill
             priority
             sizes="50vw"
